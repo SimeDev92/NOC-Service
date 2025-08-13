9a9754bc0d5bfff7831a83de1637caef763e9bf8
AlertaTrabajo <alupar9905@ieselcaminas.org>
2025-08-13 17:51:02 +0200

    fin sección 8

---
+++ src/presentation/server.ts
@@ -0,0 +1,23 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+
+    public static start() {
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com';
+
+                new CheckService(
+                    () => console.log(`${url} is ok`),
+                    (error) => console.log(error)
+                ).execute(url);
+
+            }
+
+        );
+
+    }
+}
